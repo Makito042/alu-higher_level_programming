653e62874ba16239c86a598a7d922c267f20e7c2
makito042 <makuochiokoye@gmail.com>
2024-03-01 12:02:18 -0800

    update6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    if not a_dictionary:
-        return
-    sort_by_keys = sorted(a_dictionary.keys())
-    for key in sort_by_keys:
-        print(key, ':', a_dictionary[key])
-
+def print_sorted_dictionary(my_dict):
+    for k in sorted(my_dict.keys()):
+        print("{}: {}".format(k, my_dict[k]))
